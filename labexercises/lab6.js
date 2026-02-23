1de2ab9ebd57062be4c35a5c78b066d3ef3a1ad6
BROADSTREETBULLY <hjw.personal@gmail.com>
2026-02-23 20:14:03 +1100

    Update with lab6

---
+++ labexercises/lab6.js
@@ -0,0 +1,16 @@
+const book = {
+    title : "My Brillant Friend",
+    author : "Elena Ferrante",
+    description : "My Brilliant Friend by Elena Ferrante is a critically acclaimed 2011 novel and the first part of the Neapolitan Novels quartet, focusing on the intense, lifelong, and complex friendship between Elena Greco and Lila Cerullo.",
+    numberOfPages : "500"
+}
+
+console.log(book.author)
+console.log(book.description)
+console.log(book.title)
+console.log(book.numberOfPages)
+
+book.description = "New description"
+
+console.log(book)
+
